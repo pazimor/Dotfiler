b1c3db9b873868e80bdf09551085ddec03af814e
hummel_e <eddy.hummel@epitech.eu>
2017-12-02 21:41:09 +0100

    installation from scratch

--- install.sh
+++ install.sh
@@ -1,1 +1,31 @@
+
+## install package 
+
+sudo pacman -Syu pacaur
+sudo pacman -Syu zsh
+pacaur -S i3-gaps
+pacaur -S polybar-git
+pacaur -S rofi-git
+pacaur -S compton-git
+pacaur -S papirus-icon-theme-git
+pacaur -S i3lock-color-git
+pacaur -S cava-git
+pacaur -S termite-git
+pacaur -S tilda-git
+pacaur -S oh-my-zsh-git
+pacaur -S vim
+pacaur -S feh
+
+## download fonts
+git clone https://github.com/FortAwesome/Font-Awesome.git
+
+
+## setup of Booda theme 
+feh --bg-scale ./config/wallpaper.png
+mkdir ~/.fonts
+cp -t Font-Awesome/fonts/*.ttf ~/.fonts
 cp -r configs/* ~/.config
+
+
+## install ohm y zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
